c74be0ce13f8163c7e19ae3053e5e4ae754a0357
Kevin Girón <kevin.giron000@gmail.com>
2023-04-24 16:52:46 -0600

    Clases creadas

--- ListaSimple.py
+++ ListaSimple.py
@@ -2,7 +2,30 @@ class ListaSImple:
     def __init__(self):
         self.cabeza = None
 
+    def agregar(self, dato):
+        if self.cabeza == None:
+            self.cabeza = Nodo(dato)
+        else:
+            aux = self.cabeza
+            while aux.siguiente != None:
+                aux = aux.siguiente
+            aux.siguiente = Nodo(dato)
     
+    def ordenar(self):
+        aux = self.cabeza
+        while aux.siguiente != None:
+            aux2 = aux.siguiente
+            while aux2 != None:
+                if aux.dato.valorInventario < aux2.dato.valorInventario:
+                    aux.dato, aux2.dato = aux2.dato, aux.dato
+                aux2 = aux2.siguiente
+            aux = aux.siguiente
+    
+    def imprimir(self):
+        aux = self.cabeza
+        while aux != None:
+            print(aux.dato.item)
+            aux = aux.siguiente
 
 class Nodo():
     def __init__(self, dato = None, siguiente = None):
